6d61e9cfd8aa90fca100e87413a065b50c22019a
Jesse Koldewijn <jesse@iungo.nl>
2023-05-24 11:15:03 +0200

    init: codebase creation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,21 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik"
+import type { DocumentHead } from "@builder.io/qwik-city"
 
 export default component$(() => {
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
-  );
-});
+    <div class="flex min-h-screen w-full flex-col items-center justify-center bg-black">
+      <h1 class="font-bold italic text-amber-400">Hi 👋</h1>
+      <p class="font-bold text-white">Welcome to JKinsight Qwik!</p>
+    </div>
+  )
+})
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: "JKinsight Qwik",
   meta: [
     {
-      name: 'description',
-      content: 'Qwik site description',
+      name: "description",
+      content: "A very Qwik website right?",
     },
   ],
-};
+}
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,9 @@
-import { component$, Slot } from '@builder.io/qwik';
+import { component$, Slot } from "@builder.io/qwik";
 
 export default component$(() => {
-  return <Slot />;
+	return (
+		<main class="w-full min-h-screen">
+			<Slot />
+		</main>
+	);
 });
