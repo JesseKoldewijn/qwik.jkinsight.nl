43fb9f8e3278d309b5140331fc35bc922af73ab3
Jesse Koldewijn <jesse@iungo.nl>
2023-05-25 12:13:02 +0200

    update: added qwik-image wrapper to make it behave in a easy to re-use way

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,33 @@
+import QwikImage from "@/utils/qwik-image"
 import { component$ } from "@builder.io/qwik"
 import type { DocumentHead } from "@builder.io/qwik-city"
 
+import QwikLogo from "@/images/qwik-logo.png"
+
 export default component$(() => {
   return (
-    <div class="flex min-h-screen w-full flex-col items-center justify-center bg-black">
-      <h1 class="font-bold italic text-amber-400">Hi 👋</h1>
-      <p class="font-bold text-white">Welcome to JKinsight Qwik!</p>
+    <div class="flex min-h-screen w-full flex-col items-center justify-center bg-black text-white">
+      <div class="flex w-full max-w-4xl flex-col gap-2">
+        <div class="flex w-full flex-col items-center gap-3 border-white pb-2">
+          <h1 class="text-xl font-bold italic text-amber-400">Hi 👋</h1>
+          <p class="text-2xl font-bold">Welcome to JKinsight Qwik!</p>
+        </div>
+        <div class="flex w-full flex-col items-center gap-3 border-t-2 border-white pt-2">
+          <h2 class="font-bold">ImageComponent wrapper</h2>
+          <QwikImage
+            src={QwikLogo}
+            alt="Some random image"
+            res={200}
+            height={100}
+            width={100}
+            class="border-2 border-x-amber-400 border-y-amber-600 p-3"
+          />
+          <p class="max-w-2xl text-center font-light italic">
+            This image component wrapper practically behaves and is used the
+            same way as the next/image component in Next.js
+          </p>
+        </div>
+      </div>
     </div>
   )
 })
---
+++ src/utils/qwik-image.tsx
@@ -0,0 +1,39 @@
+import { $, component$ } from "@builder.io/qwik"
+import type { ImageTransformerProps } from "qwik-image"
+import { Image, useImageProvider } from "qwik-image"
+
+export default component$(
+  (props: {
+    src: string
+    alt: string
+    res?: number
+    width?: number
+    height?: number
+    class?: string
+  }) => {
+    const imageTransformer$ = $(
+      ({ src, width, height }: ImageTransformerProps): string => {
+        // Here you can set your favourite image loaders service
+        return `${src}?height=${height}&width=${width}}&format=webp&fit=fill`
+      }
+    )
+
+    useImageProvider({
+      // You can set this prop to overwrite default values [3840, 1920, 1280, 960, 640]
+      resolutions: [props.res ? props.res : 640],
+      imageTransformer$,
+    })
+
+    return (
+      <Image
+        class={props.class ? props.class : ""}
+        layout="constrained"
+        objectFit="fill"
+        width={props.width ? props.width : 400}
+        height={props.height ? props.height : 500}
+        alt={props.alt}
+        src={props.src}
+      />
+    )
+  }
+)
